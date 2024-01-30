de96e414a3f273f9447e19fdeab9fedb63c73da5
vytdev <123163428+vytdev@users.noreply.github.com>
2024-01-30 08:10:38 +0800

    bump to 0.2.0-beta.1

--- src/catalyst/core/index.ts
+++ src/catalyst/core/index.ts
@@ -15,7 +15,7 @@ export const events = new EventManager<defaultEvents>();
 /**
  * the version of catalyst core
  */
-export const VERSION = "0.2.0-alpha.1";
+export const VERSION = "0.2.0-beta.1";
 
 // preload and export modules
 export * from "./command.js";
